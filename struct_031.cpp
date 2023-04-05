1d2113ad535d9423bdb8c3dc968a08572eb0efb8
Izalhadji31 <ryzalhadji@gmail.com>
2023-04-05 14:28:48 +0700

    Add project files.

---
+++ struct_031.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+    string NIM;
+    string Nama;
+    string Alamat;
+    int umur;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
